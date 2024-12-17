07aaa2abca8aab59cc2c5fd967fa09ca51cf5d7c
Zuzia Sobanska <zuzannasobanska97@gmail.com>
2024-12-17 19:46:34 +0100

    add react router, the home page and about us page

--- PolishWithBlondes/src/components/navbar/Navbar.tsx
+++ PolishWithBlondes/src/components/navbar/Navbar.tsx
@@ -1,12 +1,17 @@
 import './navbar.scss';
 import logo from '../../assets/logo1.png';
 import hamburger from '../../assets/hamburger.png';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src={logo} className="logo" />
       <ul>
+        <li>
+          <Link to="/">
+            <img src={logo} alt="Logo" className="logo" />
+          </Link>
+        </li>
         <li>
           <a href="">Book a lesson</a>
         </li>
@@ -25,7 +30,7 @@ export const Navbar = () => {
           </a>
         </li>
         <li>
-          <a href="">About us</a>
+          <Link to="/about">About Us</Link>
         </li>
         <li>
           <a href="">Contact</a>
@@ -37,3 +42,5 @@ export const Navbar = () => {
     </nav>
   );
 };
+
+export default Navbar;
