cb98770f176bf375db8bc7f59a091285910337ba
Zuzia Sobanska <zuzannasobanska97@gmail.com>
2025-02-11 11:46:29 +0700

    Updates

--- PolishWithBlondes/src/types.ts
+++
@@ -1,9 +0,0 @@
-export type TeacherName = 'Kasia' | 'Iza' | 'Zuzia'; //string union
-
-export type Teacher = {
-  name: TeacherName;
-  img: string;
-  description: string;
-  email: string;
-  lessonDescription: string;
-};
