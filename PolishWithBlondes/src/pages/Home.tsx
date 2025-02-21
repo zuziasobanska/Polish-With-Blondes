d8a8e1d00563a582fe1cea1022c34194295b06e5
Zuzia Sobanska <zuzannasobanska97@gmail.com>
2025-02-21 18:37:19 +0700

    deploy

--- PolishWithBlondes/src/pages/Home.tsx
+++ PolishWithBlondes/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { InfoSection } from '../components/InfoSection/InfoSection';
 import { YoutubeSection } from '../components/YoutubeSection/YoutubeSection';
 import { NewsletterSection } from '../components/NewsletterSection/NewsletterSection';
-import { Hero } from '../components/Hero/Hero';
+import { Hero } from '../components/hero/HeroComponent';
 
 const Home = () => {
   return (
